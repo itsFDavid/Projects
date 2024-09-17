b45b9c9a10bb89230252fe978d7124034e029c7f
itsFDavid <fdavid04@icloud.com>
2024-09-16 18:07:56 -0600

    Refactor bus race simulation logic, add multiple lanes, and integrate sound effects

--- Carrera-buses/index.js
+++ Carrera-buses/index.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 import readline from 'readline';
-import sound from 'sound-play';
 import player from 'play-sound';
+import inquirer from 'inquirer';
 // Configurar readline para leer desde la terminal
 const rl = readline.createInterface({
     input: process.stdin,
@@ -20,6 +20,7 @@ const COLORS = [
     "gray"
 ]
 
+
 class Bus {
     constructor(name, color,carril) {
         this.name = name;
@@ -29,7 +30,7 @@ class Bus {
     }
 
     avanzar() {
-        this.posicion += Math.random() < 0.85 ? 1 : -1;
+        this.posicion += Math.random() < 0.65 ? 1 : -.05;
         if (this.posicion < 0) this.posicion = 0;
     }
 
@@ -47,17 +48,19 @@ class Bus {
         console.log(colorFunc(`${" ".repeat(this.posicion)}     dwb=-OO-----OO-=`));
 
         console.log(chalk.yellow(linea));
+        await new Promise(resolve => setTimeout(resolve, 40));
+    }
+    async velocidad(){
+        const velocidad= parseInt(this.posicion);
+        console.log("\n".repeat(this.carril));
+        console.log(chalk.bgYellowBright(`Bus de ${this.name} va a :\t${velocidad}Km/h`));
         await new Promise(resolve => setTimeout(resolve, 50));
     }
 }
 function limpiarConsola() {
     process.stdout.write('\x1Bc');
 }
-function reproducirSonido(rutaArchivo) {
-    player().play(rutaArchivo), (err) => {
-        if (err) console.error('Error al reproducir el audio:', err);
-    };
-}
+
 
 
 async function main(buses) {
@@ -66,21 +69,26 @@ async function main(buses) {
         console.log('No hay buses para correr');
         return;
     }
-    reproducirSonido('carrera_de_buses.m4r');
+    var audio = player().play('carrera_de_buses.m4r', (err) => {
+        if (err) console.error('Error al reproducir el audio:', err);
+    });
     while (!ganador) {
         for (const bus of buses) {
             bus.avanzar();
             console.clear();
+            bus.velocidad();
             await bus.dibujar();
             
             
+            
             if (bus.posicion >= meta) {
-                ganador = bus.name;
+                ganador = bus;
+                audio.kill();
                 break;
             }
         }  
     }
-    console.log(chalk.bgYellowBright(`¡El bus de ${ganador} ha ganado la carrera!`));
+    console.log(chalk.bgGreenBright(`El bus ${ganador.name} ha ganado la carrera`));
 }
 
 
@@ -90,24 +98,29 @@ function addBus(name, color){
 }
 
 
-function preguntarNombreBus() {
+async function preguntarNombreBus() {
     limpiarConsola();
-    rl.question(chalk.red('Ingresa el nombre del bus ' + chalk.gray('(o deja vacío para iniciar la carrera): ')), (name) => {
-        if(!name){
-            rl.close();
-            main(Buses);
-        }else{
-            COLORS.forEach((color, index) => {
-                console.log(index + 1, chalk[color](color));
-            });
-            rl.question(chalk.red('Ingresa el color del bus: '), (color) => {
-                if(!color) color = 'green';
-                addBus(name,color);
-                console.clear();
-                preguntarNombreBus();
-            });
-        }
-    });
+    while (true) {
+        const { nombre } = await inquirer.prompt({
+            type: 'input',
+            name: 'nombre',
+            message: chalk.red('Ingresa el nombre del bus ') + chalk.gray('(o deja vacío para iniciar la carrera):')
+        });
+
+        if (!nombre) break;
+
+        const { color } = await inquirer.prompt({
+            type: 'list',
+            name: 'color',
+            message: chalk.green('Selecciona el color del bus:'),
+            choices: COLORS.map(color => ({
+                name: chalk[color](color),
+                value: color
+            }))
+        });
+        addBus(nombre, color);
+    }
+    main(Buses);
 }
 
 preguntarNombreBus();
