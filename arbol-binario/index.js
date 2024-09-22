19155ce5c7a89ebe672f5cb6e782d0576b29ef12
itsFDavid <fdavid04@icloud.com>
2024-09-21 22:22:40 -0600

    Refactor arbol-binario/index.js to use inquirer and chalk for user interaction

--- arbol-binario/index.js
+++ arbol-binario/index.js
@@ -1,210 +1,224 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
-class arbolBinario{
+import figlet from "figlet";  // Para darle un toque gráfico a los títulos
 
-    constructor(){
-        this.raiz = null;
-        this.left = null;
-        this.right = null;
-    }
-
-    insertar(valor){
-        if(this.raiz == null){
-            this.raiz = new Nodo(valor);
-        }else{
-            this.insertarNodo(this.raiz, valor);
-        }
-    }
-
-    insertarNodo(nodo, valor){
-        if(valor < nodo.valor){
-            if(nodo.left == null){
-                nodo.left = new Nodo(valor);
-            }else{
-                this.insertarNodo(nodo.left, valor);
-            }
-        }else if(valor > nodo.valor){
-            if(nodo.right == null){
-                nodo.right = new Nodo(valor);
-            }else{
-                this.insertarNodo(nodo.right, valor);
-            }
-        }
-    }
-
-    buscar(valor){
-        return this.buscarNodo(this.raiz, valor);
-    }
-
-    buscarNodo(nodo, valor){
-        if(nodo == null){
-            return null;
-        }else if(valor < nodo.valor){
-            return this.buscarNodo(nodo.left, valor);
-        }else if(valor > nodo.valor){
-            return this.buscarNodo(nodo.right, valor);
-        }else{
-            return nodo;
-        }
-    }
-
-    eliminar(valor){
-        this.raiz = this.eliminarNodo(this.raiz, valor);
-    }
-
-    eliminarNodo(nodo, valor){
-        if(nodo == null){
-            return null;
-        }else if(valor < nodo.valor){
-            nodo.left = this.eliminarNodo(nodo.left, valor);
-            return nodo;
-        }else if(valor > nodo.valor){
-            nodo.right = this.eliminarNodo(nodo.right, valor);
-            return nodo;
-        }else{
-            if(nodo.left == null && nodo.right == null){
-                nodo = null;
-                return nodo;
-            }
-            if(nodo.left == null){
-                nodo = nodo.right;
-                return nodo;
-            }else if(nodo.right == null){
-                nodo = nodo.left;
-                return nodo;
-            }
-            var aux = this.minimo(nodo.right);
-            nodo.valor = aux.valor;
-            nodo.right = this.eliminarNodo(nodo.right, aux.valor);
-            return nodo;
-        }
-    }
-
-
-    minimo(nodo){
-        if(nodo == null){
-            return null;
-        }else if(nodo.left == null){
-            return nodo;
-        }else{
-            return this.minimo(nodo.left);
-        }
-    }
-
-    preOrden(nodo){
-        if(nodo != null){
-            console.log(nodo.valor);
-            this.preOrden(nodo.left);
-            this.preOrden(nodo.right);
-        }
-    }
-
-    inOrden(nodo){
-        if(nodo != null){
-            this.inOrden(nodo.left);
-            console.log(nodo.valor);
-            this.inOrden(nodo.right);
-        }
-    }
-
-    postOrden(nodo){
-        if(nodo != null){
-            this.postOrden(nodo.left);
-            this.postOrden(nodo.right);
-            console.log(nodo.valor);
-        }
-    }
-
-    getRaiz(){
-        return this.raiz;
-    }
+class Nodo {
+  constructor(valor) {
+    this.valor = valor;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-class Nodo{
-    constructor(valor){
-        this.valor = valor;
-        this.left = null;
-        this.right = null;
-    }
+class ArbolBinario {
+  constructor() {
+    this.raiz = null;
+  }
 
-    getValor(){
-        return this.valor;
+  insertar(valor) {
+    const nuevoNodo = new Nodo(valor);
+    if (this.raiz === null) {
+      this.raiz = nuevoNodo;
+    } else {
+      this.insertarNodo(this.raiz, nuevoNodo);
     }
+  }
 
-    getLeft(){
-        return this.left;
+  insertarNodo(nodo, nuevoNodo) {
+    if (nuevoNodo.valor < nodo.valor) {
+      if (nodo.left === null) {
+        nodo.left = nuevoNodo;
+      } else {
+        this.insertarNodo(nodo.left, nuevoNodo);
+      }
+    } else {
+      if (nodo.right === null) {
+        nodo.right = nuevoNodo;
+      } else {
+        this.insertarNodo(nodo.right, nuevoNodo);
+      }
     }
+  }
 
-    getRight(){
-        return this.right;
-    }
+  buscar(valor) {
+    return this.buscarNodo(this.raiz, valor);
+  }
 
-    setValor(valor){
-        this.valor = valor;
+  buscarNodo(nodo, valor) {
+    if (nodo === null) {
+      return null;
     }
+    if (valor < nodo.valor) {
+      return this.buscarNodo(nodo.left, valor);
+    } else if (valor > nodo.valor) {
+      return this.buscarNodo(nodo.right, valor);
+    } else {
+      return nodo;
+    }
+  }
 
-    setLeft(left){
-        this.left = left;
-    }
+  eliminar(valor) {
+    this.raiz = this.eliminarNodo(this.raiz, valor);
+  }
 
-    setRight(right){
-        this.right = right;
+  eliminarNodo(nodo, valor) {
+    if (nodo === null) {
+      return null;
     }
+    if (valor < nodo.valor) {
+      nodo.left = this.eliminarNodo(nodo.left, valor);
+      return nodo;
+    } else if (valor > nodo.valor) {
+      nodo.right = this.eliminarNodo(nodo.right, valor);
+      return nodo;
+    } else {
+      if (nodo.left === null && nodo.right === null) {
+        return null;
+      }
+      if (nodo.left === null) {
+        return nodo.right;
+      }
+      if (nodo.right === null) {
+        return nodo.left;
+      }
+      const aux = this.minimo(nodo.right);
+      nodo.valor = aux.valor;
+      nodo.right = this.eliminarNodo(nodo.right, aux.valor);
+      return nodo;
+    }
+  }
+
+  minimo(nodo) {
+    if (nodo.left === null) {
+      return nodo;
+    }
+    return this.minimo(nodo.left);
+  }
+
+  preOrden(nodo) {
+    if (nodo !== null) {
+      console.log(nodo.valor);
+      this.preOrden(nodo.left);
+      this.preOrden(nodo.right);
+    }
+  }
+
+  inOrden(nodo) {
+    if (nodo !== null) {
+      this.inOrden(nodo.left);
+      console.log(nodo.valor);
+      this.inOrden(nodo.right);
+    }
+  }
+
+  postOrden(nodo) {
+    if (nodo !== null) {
+      this.postOrden(nodo.left);
+      this.postOrden(nodo.right);
+      console.log(nodo.valor);
+    }
+  }
+
+  getRaiz() {
+    return this.raiz;
+  }
+
+  imprimirArbol(nodo = this.raiz, prefijo = '', esIzquierdo = true) {
+
+    if (nodo !== null) {
+        // Llamada recursiva en el subárbol derecho primero (para que se imprima en la parte superior)
+        this.imprimirArbol(nodo.right, prefijo + (esIzquierdo ? "│   " : "    "), false);
+
+        // Imprimir el nodo actual
+        console.log(prefijo + (esIzquierdo ? "└── " : "┌── ") + nodo.valor);
+
+        // Llamada recursiva en el subárbol izquierdo
+        this.imprimirArbol(nodo.left, prefijo + (esIzquierdo ? "    " : "│   "), true);
+    }
+}
 }
 
 const main = async () => {
-    const {arbol} = await inquirer.prompt({
-        type: 'input',
-        name: 'arbol',
-        message: chalk.bgCyan('Ingrese el valor del nodo a insertar')
-    });
-    const arb= new arbolBinario();
-    while (arbol){
-        const {opcion} = await inquirer.prompt({
-            type: 'list',
-            name: 'opcion',
-            message: chalk.bgCyan('Seleccione una opcion'),
-            choices: ['Insertar', 'Buscar', 'Eliminar', 'PreOrden', 'InOrden', 'PostOrden', 'Salir']
-        });
+  console.log(chalk.yellow(figlet.textSync('Arbol Binario')));
+  const arbol = new ArbolBinario();
 
-        switch(opcion){
-            case 'Insertar':
-                var valor = await inquirer.prompt({
-                    type: 'input',
-                    name: 'valor',
-                    message: chalk.bgBlue('Ingrese el valor del nodo a insertar')
-                });
-                arb.insertar(valor);
-                break;
-            case 'Buscar':
-                var valor = await inquirer.prompt({
-                    type: 'input',
-                    name: 'valor',
-                    message: chalk.bgBlue('Ingrese el valor del nodo a insertar')
-                });
-                arb.buscar(valor);
-                break;
-            case 'Eliminar':
-                var valor = await inquirer.prompt({
-                    type: 'input',
-                    name: 'valor',
-                    message: chalk.bgBlue('Ingrese el valor del nodo a insertar')
-                });
-                arb.eliminar(valor);
-                break;
-            case 'PreOrden':
-                arb.preOrden(arb.getRaiz());
-                break;
-            case 'InOrden':
-                arb.inOrden(arb.getRaiz());
-                break;
-            case 'PostOrden':
-                arb.postOrden(arb.getRaiz());
-                break;
-            case 'Salir':
-                return;
+  let continuar = true;
+  while (continuar) {
+    const { opcion } = await inquirer.prompt({
+      type: 'list',
+      name: 'opcion',
+      message: chalk.bgGreenBright('Seleccione una opción'),
+      choices: [
+        'Insertar', 
+        'Buscar', 
+        'Eliminar', 
+        'PreOrden', 
+        'InOrden', 
+        'PostOrden', 
+        'Imprimir Árbol', 
+        'Salir'
+    ]
+    });
+
+    switch (opcion) {
+      case 'Insertar':
+        const { valorInsertar } = await inquirer.prompt({
+          type: 'input',
+          name: 'valorInsertar',
+          message: chalk.bgBlue('Ingrese el valor del nodo a insertar')
+        });
+        arbol.insertar(parseInt(valorInsertar));
+        console.log(chalk.green(`Nodo ${valorInsertar} insertado`));
+        break;
+
+      case 'Buscar':
+        const { valorBuscar } = await inquirer.prompt({
+          type: 'input',
+          name: 'valorBuscar',
+          message: chalk.bgBlue('Ingrese el valor del nodo a buscar')
+        });
+        const nodo = arbol.buscar(parseInt(valorBuscar));
+        if (nodo) {
+          console.log(chalk.green(`Nodo encontrado: ${nodo.valor}`));
+        } else {
+          console.log(chalk.red('Nodo no encontrado'));
         }
+        break;
+
+      case 'Eliminar':
+        const { valorEliminar } = await inquirer.prompt({
+          type: 'input',
+          name: 'valorEliminar',
+          message: chalk.bgBlue('Ingrese el valor del nodo a eliminar')
+        });
+        arbol.eliminar(parseInt(valorEliminar));
+        console.log(chalk.green(`Nodo ${valorEliminar} eliminado`));
+        break;
+
+      case 'PreOrden':
+        console.log(chalk.yellow('Recorrido PreOrden:'));
+        arbol.preOrden(arbol.getRaiz());
+        break;
+
+      case 'InOrden':
+        console.log(chalk.yellow('Recorrido InOrden:'));
+        arbol.inOrden(arbol.getRaiz());
+        break;
+
+      case 'PostOrden':
+        console.log(chalk.yellow('Recorrido PostOrden:'));
+        arbol.postOrden(arbol.getRaiz());
+        break;
+
+      case 'Imprimir Árbol':
+        console.log(chalk.yellow('Árbol binario:'));
+        arbol.imprimirArbol();
+        break;
+      case 'Salir':
+        continuar = false;
+        break;
     }
-}
+  }
+};
 
 main();
