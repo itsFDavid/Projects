7d307d1902188d6af35ca1ace688396133357f68
itsFDavid <FDavid04@icloud.com>
2024-10-19 23:42:28 -0600

    Refactor index.js to remove unused imports and middleware

--- App-Models-IA/backend/index.js
+++ App-Models-IA/backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const authRoutes = require('./src/routes/authRoutes');
 const errorHandler = require('./src/middleware/errorMiddleware');
 
+
 const app = express();
 app.use(express.json());
 
