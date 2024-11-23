4019588d09aa078b11e23282d7b36cd1961dec0b
itsFDavid <FDavid04@icloud.com>
2024-11-22 23:19:19 -0600

    Add models utility with IModel type and predefined data

---
+++ App-Models-IA/app-next/src/app/utils/models.ts
@@ -0,0 +1,19 @@
+type IModel = {
+  nombre: string;
+  descripcion: string;
+};
+
+export const MODELS: IModel[] = [
+  {
+    nombre: "Clasificación",
+    descripcion: "Clasificación de datos",
+  },
+  {
+    nombre: "Regresión",
+    descripcion: "Regresión de datos",
+  },
+  {
+    nombre: "Agrupación",
+    descripcion: "Agrupación de datos",
+  },
+];
