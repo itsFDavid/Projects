5154cba14f9cbf884d96e787519d3acc5b96b052
itsFDavid <FDavid04@icloud.com>
2024-11-22 23:19:23 -0600

    Enhance navigation component with toggle functionality and update links

--- App-Models-IA/app-next/src/app/components/nav.tsx
+++ App-Models-IA/app-next/src/app/components/nav.tsx
@@ -1,16 +1,49 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import display from "@src/page.module.css";
 
 export default function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className={display.nav}>
-      <h3 className={display.description_nav}>Prueba Modelos De ML</h3>
-      <ul>
+      <Link href="/" className={display.description_nav}>
+        Prueba Modelos De ML
+      </Link>
+      <button className={display.menuBtn} onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={`${display.navLinks} ${menuOpen ? display.open : ""}`}>
+        <button
+          className={`${display.closeBtn} ${menuOpen ? display.active : ""}`}
+          onClick={closeMenu}
+        >
+          {menuOpen ? "Cerrar" : ""}
+        </button>
         <li>
-          <Link href="/">Home</Link>
+          <Link
+            href="/login"
+            className={`${display.link_nav} ${display.link_login}`}
+          >
+            Iniciar Sesión
+          </Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link
+            href="/register"
+            className={`${display.link_nav} ${display.link_register}`}
+          >
+            Registrar
+          </Link>
         </li>
       </ul>
     </nav>
