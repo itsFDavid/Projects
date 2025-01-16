179d83a91e1af1b21d5468929ad9d8efd6fb88e1
itsFDavid <FDavid04@icloud.com>
2025-01-15 20:15:09 -0600

    add CORS configuration to main application bootstrap

--- API-Nest-Tienda/tienda-don-pepe/src/main.ts
+++ API-Nest-Tienda/tienda-don-pepe/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,20 @@ async function bootstrap() {
       transform: true,
     })
   )
+
+  const configService = app.get(ConfigService);
+
+  // Obtén los orígenes permitidos desde la variable de entorno
+  const corsOrigins = configService
+    .get<string>('CORS_ORIGINS')
+    ?.split(',') || [];
+
+  app.enableCors({
+    origin: corsOrigins.length ? corsOrigins : '*', // Permite todos si no se define
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
     // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('API de tiendas don pepe 🏪')
@@ -23,6 +38,6 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.API_PORT ?? 3000);
 }
 bootstrap();
