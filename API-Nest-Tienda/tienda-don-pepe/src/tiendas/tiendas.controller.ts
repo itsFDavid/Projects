706d9bd6e3421e3a10b10a37f1dc6f9b48f30b95
itsFDavid <FDavid04@icloud.com>
2025-04-21 08:56:50 -0600

    implement role-based access control in TiendasController; enhance TiendasModule to include AuthModule

--- API-Nest-Tienda/tienda-don-pepe/src/tiendas/tiendas.controller.ts
+++ API-Nest-Tienda/tienda-don-pepe/src/tiendas/tiendas.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { TiendasService } from './tiendas.service';
 import { CreateTiendaDto } from './dto/create-tienda.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { ApiTags, ApiResponse, ApiBody, ApiOperation, ApiQuery, ApiParam } from '@nestjs/swagger';
+import { UserRoleGuard } from 'src/auth/guards/user-role.guard';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RoleProtected } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @ApiTags('tiendas')
 @Controller('tiendas')
@@ -14,6 +18,8 @@ export class TiendasController {
   @ApiBody({ type: CreateTiendaDto })
   @ApiResponse({ status: 201, description: 'Tienda creada' })
   @ApiResponse({ status: 400, description: 'Error en los datos enviados' })
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.ADMIN)
   create(@Body() createTiendaDto: CreateTiendaDto) {
     return this.tiendasService.create(createTiendaDto);
   }
@@ -21,6 +27,8 @@ export class TiendasController {
   @Get('seed')
   @ApiOperation({ summary: 'Crear tiendas de prueba' })
   @ApiResponse({ status: 201, description: 'Tiendas creadas' })
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.ADMIN)
   seed() {
     return this.tiendasService.seed();
   }
@@ -30,6 +38,8 @@ export class TiendasController {
   @ApiQuery({ type: PaginationDto })
   @ApiResponse({ status: 200, description: 'Tiendas encontradas' })
   @ApiResponse({ status: 400, description: 'Error en los datos enviados' })
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.USER, ValidRoles.ADMIN)
   findAll(@Query() paginationDto: PaginationDto) {
     return this.tiendasService.findAll(paginationDto);
   }
@@ -40,6 +50,8 @@ export class TiendasController {
   @ApiResponse({ status: 200, description: 'Tienda encontrada' })
   @ApiResponse({ status: 404, description: 'Tienda no encontrada' })
   @ApiResponse({ status: 400, description: 'Error en los datos enviados' })
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.USER, ValidRoles.ADMIN)
   findOne(@Param('term') id: string) {
     return this.tiendasService.findOne(id);
   }
@@ -51,6 +63,8 @@ export class TiendasController {
   @ApiResponse({ status: 200, description: 'Tienda actualizada' })
   @ApiResponse({ status: 404, description: 'Tienda no encontrada' })
   @ApiResponse({ status: 400, description: 'Error en los datos enviados' })
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.ADMIN)
   update(@Param('id', ParseIntPipe) id: number, @Body() updateTiendaDto: CreateTiendaDto) {
     return this.tiendasService.update(id, updateTiendaDto);
   }
@@ -61,6 +75,8 @@ export class TiendasController {
   @ApiResponse({ status: 200, description: 'Tienda eliminada' })
   @ApiResponse({ status: 404, description: 'Tienda no encontrada' })
   @ApiResponse({ status: 400, description: 'Error en los datos enviados' })
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.ADMIN)
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.tiendasService.remove(id);
   }
--- API-Nest-Tienda/tienda-don-pepe/src/tiendas/tiendas.module.ts
+++ API-Nest-Tienda/tienda-don-pepe/src/tiendas/tiendas.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TiendasService } from './tiendas.service';
 import { TiendasController } from './tiendas.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tienda } from './entities/tienda.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Tienda])],
+  imports:[TypeOrmModule.forFeature([Tienda]), forwardRef(() => AuthModule)],
   controllers: [TiendasController],
   providers: [TiendasService],
   exports: [TypeOrmModule]
